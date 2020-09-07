a74cad5eeaceb4fbda7f490b03938c777e2505da
guasam <guasam@outlook.in>
2020-09-07 17:23:52 +0530

    raycast bulb

--- main.gd
+++ main.gd
@@ -21,11 +21,31 @@ func _ready() -> void:
 		var a = Vector2(randi() % int(screenSize.x), randi() % int(screenSize.y))
 		var b = Vector2(randi() % int(screenSize.x), randi() % int(screenSize.y))
 		w.setPosition(a, b)
-		w.color = Color.red
+		w.color = Color.white
 		walls.insert(n, w)
 
+	# boundary walls
+	var b_top = Beam.new()
+	var b_right = Beam.new()
+	var b_bottom = Beam.new()
+	var b_left = Beam.new()
+	# boundary positions
+	b_top.setPosition(Vector2(0,10), Vector2(screenSize.x, 10))
+	b_right.setPosition(Vector2(screenSize.x - 10, 0), Vector2(screenSize.x - 10, screenSize.y - 10))
+	b_bottom.setPosition(Vector2(0,screenSize.y - 10), Vector2(screenSize.x - 10, screenSize.y - 10))
+	b_left.setPosition(Vector2(10,0), Vector2(10, screenSize.y - 10))
 
-	lumen = Lumen.new(centerPos, 10, Color.gray)
+
+	# append to walls
+	walls.append(b_top)
+	walls.append(b_right)
+	walls.append(b_bottom)
+	walls.append(b_left)
+
+
+	lumen = Lumen.new(centerPos, 10, Color.white)
+
+
 
 func _draw() -> void:
 	lumen.pos = get_local_mouse_position() # follow mouse
@@ -33,18 +53,18 @@ func _draw() -> void:
 	draw_lumen(lumen)
 
 	# set wall position
-	wall.setPosition(centerPos + Vector2(200, -200), centerPos + Vector2(200, 200)) # default
+#	wall.setPosition(centerPos + Vector2(200, -200), centerPos + Vector2(200, 200)) # default
 #	wall.setPosition(centerPos + Vector2(500, -200), centerPos + Vector2(100, 200))
-	wall.color = Color.white
-	draw_beam(wall)
+#	wall.color = Color.white
+#	draw_beam(wall)
 
-	for n in range(0, 359, 10):
+	for n in range(0, 360):
 		var b = Beam.new()
-		b.setPosition(lumen.pos, centerPos + Vector2(get_viewport_rect().size.x * 0.5, 0).rotated(deg2rad(n)))
-		# wall intersection
-#		var intersection = Beam.intersection(b, wall) # default
+		b.setPosition(lumen.pos, Vector2(1024, 600).rotated(deg2rad(n)))
+		#b.setPosition(lumen.pos, centerPos + Vector2(get_viewport_rect().size.x * 0.5, 0).rotated(deg2rad(n)))
+
 		var record = INF
-		var closest = null
+		var closest:Vector2 = Vector2.ZERO
 
 		for w in walls:
 			var intersection = Beam.intersection(b, w)
@@ -57,9 +77,11 @@ func _draw() -> void:
 
 		if closest:
 			b.color = Color.white
+			b.color.a = 0.25
 			b.posB = closest
-			# draw beam
-			draw_beam(b)
+
+		# draw beam
+		draw_beam(b)
 		# free up beam object
 		b.free()
 
@@ -73,7 +95,7 @@ func _process(delta: float) -> void:
 	pass
 
 func draw_beam(beam: Beam) -> void:
-	draw_line(beam.posA, beam.posB, beam.color, 1)
+	draw_line(beam.posA, beam.posB, beam.color, 1, true)
 
 func draw_lumen(lumen: Lumen) -> void:
 	draw_circle(lumen.pos, lumen.radius, lumen.color)
